95d5ad5bec14e9da0600f14921b11484c959ec76
ryq <1152416230@qq.com>
2024-09-06 20:08:38 +0800

    first commit

---
+++ my-map-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import MapComponent from './MapComponent';
+
+function App() {
+  return (
+    <div className="App" style={{ height: '100vh' }}>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
